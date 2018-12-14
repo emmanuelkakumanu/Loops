e5c4d18c5025e13a718771861b7427594055eb56
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-12-14 11:16:16 +0530

    Create 5.java

---
+++ 5.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		int c=s.nextInt();
+		if(a>b && a>c)
+		System.out.println("a is largest : "+a);
+		else if(b>a && b>c)
+		System.out.println("b is largest : "+b);
+		else
+		System.out.println("c is largest : "+c);
+	}
+}
