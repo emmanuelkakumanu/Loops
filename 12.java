a8489d24331a11c9b4874f4676297f94190f47c7
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-12-14 11:33:47 +0530

    Create 12.java

---
+++ 12.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		char ch='F';
+		switch(ch)
+		{
+			case 'M' : System.out.println("Male");break;
+			case 'F' : System.out.println("Female");break;
+		}
+	}
+}
